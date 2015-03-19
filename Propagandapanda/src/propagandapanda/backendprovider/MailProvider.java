437614caf6f163c0902d3d9e4b0cde45afe3a707
Michi <Michi@10.0.0.9>
2015-03-19 01:41:37 +0100

    JavaMail Documentation added

--- Propagandapanda/src/propagandapanda/backendprovider/MailProvider.java
+++ Propagandapanda/src/propagandapanda/backendprovider/MailProvider.java
@@ -45,5 +45,48 @@ public class MailProvider extends BackendProvider{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    
+    private int sendMail(){
+        try
+        {
+            Properties props = System.getProperties();
+              // -- Attaching to default Session, or we could start a new one --
+              props.put("mail.transport.protocol", "smtp" );
+              props.put("mail.smtp.starttls.enable","true" );
+              props.put("mail.smtp.host",smtpServ);
+              props.put("mail.smtp.auth", "true" );
+              Authenticator auth = new SMTPAuthenticator();
+              Session session = Session.getInstance(props, auth);
+              // -- Create a new message --
+              Message msg = new MimeMessage(session);
+              // -- Set the FROM and TO fields --
+              msg.setFrom(new InternetAddress(from));
+              msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+              msg.setSubject(subject);
+              msg.setText(message);
+              // -- Set some other header information --
+              msg.setHeader("MyMail", "Mr. XYZ" );
+              msg.setSentDate(new Date());
+              // -- Send the message --
+              Transport.send(msg);
+              System.out.println("Message sent to"+to+" OK." );
+              return 0;
+        }
+        catch (Exception ex)
+        {
+          ex.printStackTrace();
+          System.out.println("Exception "+ex);
+          return -1;
+        }
+  }
+
+// Also include an inner class that is used for authentication purposes
+
+private class SMTPAuthenticator extends javax.mail.Authenticator {
+        @Override
+        public PasswordAuthentication getPasswordAuthentication() {
+            String username =  "Java.Mail.CA@gmail.com";           // specify your email id here (sender's email id)
+            String password = "javamail";                                      // specify your password here
+            return new PasswordAuthentication(username, password);
+        }
+  }
 }
